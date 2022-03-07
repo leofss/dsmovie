15a69a3b24988dfcfbde4de844983bee857e58a9
leonardo fernandes <leofernandess11@gmail.com>
2022-03-07 14:18:23 -0300

    Projeto criado

---
+++ backend/src/test/java/com/devsup/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsup.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
