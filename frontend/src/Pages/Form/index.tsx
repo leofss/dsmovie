4908f7e6848c652f4ffa5dd0e62e8b63a18d5835
leonardo fernandes <leofernandess11@gmail.com>
2022-03-07 16:39:57 -0300

    Navigation buttons

--- frontend/src/Pages/Form/index.tsx
+++ frontend/src/Pages/Form/index.tsx
@@ -1,5 +1,6 @@
 import App from "App";
 import './styles.css';
+import { Link } from "react-router-dom";
 
 function Form() {
     const movie = {
@@ -34,7 +35,9 @@ function Form() {
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
                 </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
             </div >
         </div >
     );
