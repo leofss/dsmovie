4152fb70057435c13a5159d78c790a496b3ebf6a
leonardo fernandes <leofernandess11@gmail.com>
2022-03-07 16:29:36 -0300

    Movie Card

--- frontend/src/Pages/listing/index.tsx
+++ frontend/src/Pages/listing/index.tsx
@@ -1,10 +1,37 @@
 import App from "App";
 import Paginatios from "components/Paginatios";
+import MovieStars from "components/MovieStars";
+import MovieScores from "components/MovieScore";
+import MovieCard from "components/MovieCard";
 
 function Listing() {
-    return(
-        <Paginatios />
-        
+    return (
+        <>
+            <Paginatios />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                    <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                    <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                    <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                    <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                    <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                    <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
+
     );
 }
 
